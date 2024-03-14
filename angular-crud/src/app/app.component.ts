037992d30b368b288a2e5588617bf0118434f541
Yuri Cesar <ycesar4273@gmail.com>
2024-03-14 16:29:39 -0300

    Listando cursos

--- angular-crud/src/app/app.component.ts
+++ angular-crud/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule
+  ],
+  providers: [
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ angular-crud/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+import { Course } from '../model/course';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
+
+@Component({
+  selector: 'app-courses',
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
+  ],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent {
+
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'front-end'}
+  ];
+  displayedColumns = ['name', 'category']
+
+}
