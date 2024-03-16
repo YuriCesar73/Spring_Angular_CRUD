a8f977d1c6cde4e110b52bc9db2e2478d524fa06
Yuri Cesar <ycesar4273@gmail.com>
2024-03-16 19:07:04 -0300

    Icones adicionados na tabela e componente form criado.

--- angular-crud/src/app/courses/courses/courses.component.ts
+++ angular-crud/src/app/courses/courses/courses.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable, catchError, of } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
+import { CategoryPipe } from '../../shared/pipes/category.pipe';
+
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { CoursesService } from '../services/courses.service';
-import { Observable, catchError, of } from 'rxjs';
-import { HttpClientModule } from '@angular/common/http';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { CommonModule } from '@angular/common';
-import { MatDialog } from '@angular/material/dialog';
-import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
-import { MatIconModule } from '@angular/material/icon';
-import { CategoryPipe } from '../../shared/pipes/category.pipe';
 
 
 
@@ -28,7 +31,8 @@ import { CategoryPipe } from '../../shared/pipes/category.pipe';
     CommonModule,
     ErrorDialogComponent,
     MatIconModule,
-    CategoryPipe
+    CategoryPipe, 
+    MatButtonModule
   ],
   providers: [
     CoursesService, 
@@ -39,7 +43,7 @@ import { CategoryPipe } from '../../shared/pipes/category.pipe';
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category']
+  displayedColumns = ['name', 'category', 'actions']
 
   constructor(
     private coursesService: CoursesService,
