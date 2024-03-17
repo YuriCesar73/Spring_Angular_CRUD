f59a72c3c5975e37a2b2202d369dbc73fc93c505
Yuri Cesar <ycesar4273@gmail.com>
2024-03-17 13:44:05 -0300

    Tela de formulário feita.

--- angular-crud/src/app/courses/course-form/course-form.component.ts
+++ angular-crud/src/app/courses/course-form/course-form.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
 
 @Component({
   selector: 'app-course-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss'
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit{
+
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(){
+
+  }
+
+  onCancel(){
+    
+  }
 
 }
