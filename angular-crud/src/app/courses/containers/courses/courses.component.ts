2dd0eab82f0305151552661083ba48eb00496a23
Yuri Cesar <ycesar4273@gmail.com>
2024-03-21 16:02:32 -0300

    removido os imports.

--- angular-crud/src/app/courses/containers/courses/courses.component.ts
+++ angular-crud/src/app/courses/containers/courses/courses.component.ts
@@ -7,48 +7,28 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
 import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
-// import { CategoryPipe } from '../../shared/pipes/category.pipe';
-
 
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialog } from '@angular/material/dialog';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CoursesListComponent } from '../../components/courses-list/courses-list.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { error } from 'console';
-
-
-
-
-
-
 
 @Component({
   selector: 'app-courses',
   standalone: true,
   imports: [
-    // MatTableModule,
     MatCardModule,
     MatToolbarModule,
     HttpClientModule,
     MatProgressSpinnerModule,
     CommonModule,
     ErrorDialogComponent,
-    // MatIconModule,
-    // CategoryPipe, 
-    // MatButtonModule,
     CoursesListComponent,
     MatDialogModule
   ],
-  // providers: [
-  //   CoursesService,
-  //   Router 
-  // ], NÃO PRECISA UTILIZAR
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 }) 
@@ -72,7 +52,6 @@ export class CoursesComponent implements OnInit {
 
   onError(errorMessage: string): void {
     this.dialog.open(ErrorDialogComponent, {
-      //width: '250px',
       data: errorMessage
     });
   }
@@ -86,18 +65,19 @@ export class CoursesComponent implements OnInit {
   }
 
   onRemove(course: Course){
-    //const dialogRef = this.dialog.open(ConfirmDialog)
     console.log(course._id)
     this.coursesService.remove(course._id).subscribe(() => {
-      this.refres();
+      () => {this.refres();
       this.snackBar.open('Curso removido com sucesso!', 'X', {
         duration: 4000,
         verticalPosition: 'top',
         horizontalPosition: 'center'
       })
-    },
-      () => this.onError('Erro ao tentar remover curso')
-    );
+    }
+  },
+    () => this.onError('Erro ao tentar remover curso')
+  );
+    
   }
 
 
